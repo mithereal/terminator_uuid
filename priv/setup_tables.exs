e98ad555baca97de377ccd15e6ac29b7be7046f0
mithereal <mithereal@gmail.com>
2023-01-12 03:12:21 -0700

    use ecto uuid for schema ids

--- priv/setup_tables.exs
+++ priv/setup_tables.exs
@@ -22,8 +22,8 @@ defmodule Terminator.Repo.Migrations.SetupTables do
 
     create table(:terminator_performers_roles, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:performer_id, references(:terminator_performers))
-      add(:role_id, references(:terminator_roles))
+      add(:performer_id, references(:terminator_performers), type: :uuid)
+      add(:role_id, references(:terminator_roles), type: :uuid)
 
       timestamps()
     end
